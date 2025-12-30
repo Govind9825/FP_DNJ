fee5788dd671b74c5b71f60f9363f2ef41c53bfb
Govind Bhatter <govindbhatter@gmail.com>
2025-12-31 02:40:33 +0530

    fixed apis

--- Backend/db.js
+++ Backend/db.js
@@ -4,7 +4,6 @@ const connectToMongo = async () => {
   const mongoURI = process.env.MONGO_URI
   try {
     await mongoose.connect(mongoURI);
-    console.log("DB connected!!")
   } catch (error) {
     console.error("Error connecting to mongoose:", error);
   }
