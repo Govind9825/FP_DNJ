8b957c5efa20deec3e26c47ff87202b8c428dd7c
Govind Bhatter <govindbhatter@gmail.com>
2026-01-04 00:22:46 +0530

    testing automation

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config({ path: "../.env" });
 
-
+//Testing automation
 
 connectToMongo(); // Connect to MongoDB
 
