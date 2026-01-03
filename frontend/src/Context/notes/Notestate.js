8480521b93e573ead55584a422aed3b20a4af2e5
Govind Bhatter <govindbhatter@gmail.com>
2026-01-04 01:40:49 +0530

    fixed urls

--- frontend/src/Context/notes/Notestate.js
+++ frontend/src/Context/notes/Notestate.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = "http://98.70.24.63";
   const { showAlert } = props;
   const [notes, setNotes] = useState([]);
 
